ac261091a93e891b0fa9f88b320d3f2ace9316b2
Felix Kimmel <felix.kimmel@web.de>
2025-01-24 18:19:35 +0100

    added qalculate

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@
     nextcloud-client
     prusa-slicer
     ltex-ls-plus
+    qalculate-qt
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
