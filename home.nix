d6375ac4ca62894f343a0a6981056ebb76df081e
Felix Kimmel <felix.kimmel@web.de>
2025-01-26 19:01:41 +0100

    added Spotify

--- home.nix
+++ home.nix
@@ -42,6 +42,7 @@
     ltex-ls-plus
     qalculate-qt
     gcr
+    spotify
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
