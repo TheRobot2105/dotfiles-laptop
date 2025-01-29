e713eddcf7219674a4a5df6a4aaf0ab51af1f436
Felix Kimmel <91203390+TheRobot2105@users.noreply.github.com>
2025-01-29 15:02:07 +0100

    added element client for matrix

--- home.nix
+++ home.nix
@@ -44,6 +44,7 @@
     gcr
     spotify
     github-desktop
+    element-desktop
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
