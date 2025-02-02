58c66bd652f9fe8271d46d061d2698dfc758fcb9
Felix Kimmel <91203390+TheRobot2105@users.noreply.github.com>
2025-02-02 21:21:31 +0100

    updated plasmamanager config

--- home.nix
+++ home.nix
@@ -6,6 +6,7 @@
   imports = [
     ./user-config/vscode.nix
     ./user-config/hyprland.nix
+    ./user-config/plasma.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
