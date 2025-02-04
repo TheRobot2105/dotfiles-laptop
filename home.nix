0227c32cc54c4f4cbe71157ba9d9e6cc539e033f
Felix Kimmel <91203390+TheRobot2105@users.noreply.github.com>
2025-02-04 12:00:47 +0100

    added libreoffice and partitionmanager

--- home.nix
+++ home.nix
@@ -47,6 +47,8 @@
     #github-desktop
     element-desktop
     zoom-us
+    libreoffice
+    kdePackages.partitionmanager
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
