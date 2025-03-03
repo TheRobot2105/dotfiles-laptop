da79c447f2d2881d4b1fd411da2b7776fb19ae0b
Felix Kimmel <91203390+TheRobot2105@users.noreply.github.com>
2025-03-03 14:37:20 +0100

    Create howdy.nix

---
+++ howdy.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  stdenv,
+  fetchgit,
+  libevdev,
+  python312,
+  python312Packages,
+  ninja,
+  meson,
+  inih,
+  
+}:
+stdenv.mkDerivation {
+  pname = "howdy";
+  version = "v2.6.1";
+  src = fetchgit {
+    url = "https://github.com/boltgolt/howdy.git";
+    sha256 = "sha256-y/BVj6DdnppIegAEm2FtrOdiqF23Q+U6v2EZ4A9H7iU=";
+  };
+
+postPatch = ''
+      substituteInPlace meson.options \
+      --replace-fail "option('python_path', type: 'string', value: '/usr/bin/python', description: 'Set the path to the python executable')" "option('python_path', type: 'string', value: '${python312}/bin/python3.12', description: 'Set the path to the python executable')"       
+'';
+
+  nativeBuildInputs = [
+    libevdev
+    python312
+    #python312Packages.pip
+    #python312Packages.setuptools
+    #python312Packages.wheel
+    ninja
+    meson
+    inih
+  ];
+
+
+  buildPhase = ''
+   
+    meson setup build
+    meson compile -C build
+    meson install -C build 
+  '';
+
+  meta = with lib; {
+    license = licenses.mit;
+    description = "";
+    homepage = "";
+    maintainers = with maintainers; [ therobot2105 ];
+  };
+}
