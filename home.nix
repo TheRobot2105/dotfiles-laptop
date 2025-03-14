2464c3f165599a02cc9c4fa9bf4979e28e952707
installer <installer@gmail.com>
2025-03-14 17:12:09 +0100

    removed steam

--- home.nix
+++ home.nix
@@ -49,7 +49,6 @@
     zoom-us
     libreoffice
     obsidian
-    steam
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
