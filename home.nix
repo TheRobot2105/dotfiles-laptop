cdf1a0d5648c3439688369a1a5e907dfed8d346c
installer <installer@gmail.com>
2025-03-21 23:44:43 +0100

    added filelite

--- home.nix
+++ home.nix
@@ -50,7 +50,7 @@
     libreoffice
     obsidian
     qbittorrent
-
+    kdePackages.filelight
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
