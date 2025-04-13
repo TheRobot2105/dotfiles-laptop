17fd3cebc1d85c59778b42d114ddaf6557e6f07e
Felix Kimmel <felix.kimmel@web.de>
2025-04-13 13:57:06 +0200

    added jabref

--- home.nix
+++ home.nix
@@ -49,6 +49,7 @@
     qbittorrent
     kdePackages.filelight
     curseofwar
+    jabref
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
