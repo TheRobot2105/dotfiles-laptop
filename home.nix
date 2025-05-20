a45dba34eed5793c99d810c8b6391d3a00c496d5
Felix Kimmel <felix.kimmel@web.de>
2025-05-20 20:50:20 +0200

    added drawio

--- home.nix
+++ home.nix
@@ -62,6 +62,7 @@
     networkmanagerapplet
     waybar
     gnome-multi-writer
+    drawio
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
