ea2b4a8033972e9580f616695c97f0d45cfe8ca1
Felix Kimmel <felix.kimmel@web.de>
2025-05-28 13:34:38 +0200

    updated lockfile

--- home.nix
+++ home.nix
@@ -63,6 +63,7 @@
     waybar
     gnome-multi-writer
     drawio
+    vlc
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
