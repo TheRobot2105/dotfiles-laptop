1d98d3b09a8e193a250e1f6b5fcfa5ffda9d306b
Felix Kimmel <felix.kimmel@web.de>
2025-06-19 12:50:26 +0200

    updated lockfile

--- user-config/vscode.nix
+++ user-config/vscode.nix
@@ -21,7 +21,7 @@
         pkief.material-icon-theme
         #platformio.platformio-vscode-ide
         #ms-vscode.cpptools
-        eamodio.gitlens
+        #eamodio.gitlens
       ];
       userSettings = {
         "extensions.autoUpdate" = false;
