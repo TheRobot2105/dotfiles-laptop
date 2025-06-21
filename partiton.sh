5f2afbdc28841fb832bb05b5299d66a7eded4baa
Felix Kimmel <felix.kimmel@web.de>
2025-06-21 21:33:16 +0200

    corrected filename

--- partiton.sh
+++ partiton.sh
@@ -2,4 +2,4 @@
 echo -n "Password für LUKS:"
 read -s password
 echo -n "$password" > /tmp/luks-password
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk-config.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix
