4e4525b4794ac17ff72da83fe2089753e9246337
Felix Kimmel <felix.kimmel@web.de>
2025-06-21 23:30:15 +0200

    corrected shellscript

--- partiton-install.sh
+++ partiton-install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 rm -rf /tmp/luks-password
 echo -n "Password für LUKS:"
-read -s password
+read -sr password
 echo -n "$password" > /tmp/luks-password
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix
 echo -n "Ready for Install?(yes/no):"
 read ready
-if [["$ready" = "yes"]]; then
+if [[ "$ready" = "yes" ]]; then
     sudo nixos-install --root /mnt --flake .#nixos-laptop
+fi
