a8c1686515aa6cdb41878a218dff6175ad17c2d7
Felix Kimmel <felix.kimmel@web.de>
2025-06-23 20:20:18 +0200

    corrected options

--- configuration.nix
+++ configuration.nix
@@ -138,7 +138,6 @@
   users.users.felix = {
     isNormalUser = true;
     description = "Felix Kimmel";
-    hashedPassword = "$y$j9T$6LDxAJeogJeEiwh9.svLR1$TkQUEtU4fPOxO/lVgdWnIl439CGg6rxPQNzp0fXRQV8";
     hashedPasswordFile = config.sops.secrets.password.path;
 
     extraGroups = [
