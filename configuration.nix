a9f729d331971803433bfb26b997288d6adeaff3
Felix Kimmel <felix.kimmel@web.de>
2025-06-24 12:09:50 +0200

    updated lockfile

--- configuration.nix
+++ configuration.nix
@@ -18,11 +18,6 @@
     #./hyprland-system.nix
   ];
 
-  nix.settings = {
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
