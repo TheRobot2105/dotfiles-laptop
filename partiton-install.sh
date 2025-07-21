b382c79dc9e208d557aaf9c290f06a299bb26fed
Felix Kimmel <felix.kimmel@web.de>
2025-07-21 10:08:40 +0200

    added facter in script

--- partiton-install.sh
+++ partiton-install.sh
@@ -3,6 +3,8 @@ rm -rf /tmp/luks-password
 echo -n "Password für LUKS:"
 read -sr password
 echo -n "$password" >/tmp/luks-password
+sudo nix run --option experimental-features "nix-command flakes" nixpkgs#nixos-facter -- -o facter.json
+git add .
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix --yes-wipe-all-disks
 echo -n "Ready for Install?(yes/no):"
 read -r ready
