eb0bd36a17524291c4a89b4eaf9a7bd45d8fc706
Felix Kimmel <felix.kimmel@web.de>
2025-08-16 21:17:28 +0200

    improved script

--- partiton-install.sh
+++ partiton-install.sh
@@ -3,12 +3,13 @@ rm -rf /tmp/luks-password
 echo -n "Password für LUKS:"
 read -sr password
 echo -n "$password" >/tmp/luks-password
-sudo nix run --option experimental-features "nix-command flakes" nixpkgs#nixos-facter -- -o facter.json
-git add .
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix --yes-wipe-all-disks
+
 echo -n "Ready for Install?(yes/no):"
 read -r ready
 if [[ "$ready" = "yes" ]]; then
+    sudo nix run --option experimental-features "nix-command flakes" nixpkgs#nixos-facter -- -o facter.json
+    git add .
+    sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disko-config.nix --yes-wipe-all-disks
     sudo nixos-install --root /mnt --flake .#nixos-laptop
     sudo nixos-enter --root /mnt -c 'passwd felix'
 fi
