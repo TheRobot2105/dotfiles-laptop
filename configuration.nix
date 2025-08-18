70901baa97b65dedf2dad9e1826a757f45759b93
Felix Kimmel <felix.kimmel@web.de>
2025-08-18 23:08:24 +0200

    updated lockfile

--- configuration.nix
+++ configuration.nix
@@ -166,7 +166,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     nixd
-    texliveFull
+    texlive.combined.scheme-full
     git
     git-crypt
     filezilla
