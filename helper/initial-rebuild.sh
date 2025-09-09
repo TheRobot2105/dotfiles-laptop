c9163a4f68255002ac14fb58634669f5b026b7b1
Felix Kimmel <felix.kimmel@web.de>
2025-09-09 11:20:16 +0200

    added initial-rebuild script

---
+++ helper/initial-rebuild.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+ssh_private=/etc/ssh/ssh_host_ed25519_key
+ssh_public=/etc/ssh/ssh_host_ed25519_key.pub
+dotfiles_dir=/home/felix/.dotfiles
+if [ -f "$ssh_private" ] && [ -f "$ssh_public" ] && [ -d "$dotfiles_dir" ]; then
+	sudo nix run --option experimental-features "nix-command flakes" nixpkgs#nixos-facter -- -o facter.json
+	sudo chown felix:users facter.json
+	sudo nixos-rebuild switch --flake .#nixos-laptop
+else
+	echo "Bitte den Odnernamen kontrollieren und den Namen und Ort des ssh-keys überprüfen."
+fi
