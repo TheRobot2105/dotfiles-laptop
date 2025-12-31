cbb31aeccf48591b04ceb3d45e7b74d792f3caae
Felix Kimmel <felix.kimmel@web.de>
2025-12-31 09:55:43 +0100

    Revert "updated lockfile"
    This reverts commit 8fe72cb9ec81cccf43b000e37a02a6517668a794.

--- home.nix
+++ home.nix
@@ -72,8 +72,6 @@
       cryptomator
       kicad
       rpi-imager
-      freecad
-      jetbrains.idea-community-bin
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
