b885ea02808e248b7aeeaa8f7074af8dee7bc7dd
Felix Kimmel <felix.kimmel@web.de>
2026-01-07 14:52:37 +0100

    updated lockfile

--- home.nix
+++ home.nix
@@ -73,7 +73,7 @@
       kicad
       rpi-imager
       jabref
-
+      fresh-editor
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
