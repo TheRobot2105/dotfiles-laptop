dfdbe1c672fe9790550b847c541a1fc819109471
Felix Kimmel <felix.kimmel@web.de>
2026-01-21 16:01:39 +0100

    updated home.nix

--- home.nix
+++ home.nix
@@ -35,7 +35,7 @@
     fastfetch
     #networkmanagerapplet
     #renpy
-    bitwarden
+    bitwarden-desktop
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
