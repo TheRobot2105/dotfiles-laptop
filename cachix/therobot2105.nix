21b68e8803669da9c5a6a60e8b7984ca3463b35e
Felix Kimmel <felix.kimmel@web.de>
2026-01-29 16:25:48 +0100

    added cachix

---
+++ cachix/therobot2105.nix
@@ -0,0 +1,13 @@
+
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://therobot2105.cachix.org"
+      ];
+      trusted-public-keys = [
+        "therobot2105.cachix.org-1:RX3o93UfxJFGbsvHxthPnVYAs2FnL7H6V7N9VUh4AnQ="
+      ];
+    };
+  };
+}
