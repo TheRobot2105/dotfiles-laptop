363fe41bc9786d6e1e395cbdab1aee897fc3ed1c
Felix Kimmel <felix.kimmel@web.de>
2026-01-29 11:49:02 +0100

    modified nh config

--- home.nix
+++ home.nix
@@ -138,8 +138,6 @@
   };
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
     flake = "/home/felix/.dotfiles"; # sets NH_OS_FLAKE variable for you
   };
 
