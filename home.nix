e0c115eca66cf87ebc8a3872e88f56d4eb578c00
Felix Kimmel <felix.kimmel@web.de>
2026-02-14 17:59:51 +0100

    added teamspeak

--- home.nix
+++ home.nix
@@ -65,6 +65,7 @@
       openscad-unstable
       just
       just-lsp
+      teamspeak6-client
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
