9d0c3a8ff8878aeefd3041679910e1558b03c224
Felix Kimmel <felix.kimmel@web.de>
2026-03-17 23:20:13 +0100

    updated lockfile

--- home.nix
+++ home.nix
@@ -66,6 +66,7 @@
       just
       just-lsp
       teamspeak6-client
+      bottles
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
