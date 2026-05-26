443b1499800f466516ee6baa89cc22cd2afff4f8
Felix Kimmel <felix.kimmel@web.de>
2026-05-26 21:28:24 +0200

    disabled fhs enviroment

--- configuration.nix
+++ configuration.nix
@@ -194,35 +194,35 @@
     nixfmt-tree
     yubioath-flutter
     syncthingtray
-    (
-      let
-        base = pkgs.appimageTools.defaultFhsEnvArgs;
-      in
-      pkgs.buildFHSEnv (
-        base
-        // {
-          name = "fhs";
-          targetPkgs =
-            pkgs:
-            # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
-            # lacking many basic packages needed by most software.
-            # Therefore, we need to add them manually.
-            #
-            # pkgs.appimageTools provides basic packages required by most software.
-            (base.targetPkgs pkgs)
-            ++ (with pkgs; [
-              pkg-config
-              ncurses
-              chromium
-              python3
-              # Feel free to add more packages here if needed.
-            ]);
-          profile = "export FHS=1";
-          runScript = "bash";
-          extraOutputsToInstall = [ "dev" ];
-        }
-      )
-    )
+    #(
+    #  let
+    #    base = pkgs.appimageTools.defaultFhsEnvArgs;
+    #  in
+    #  pkgs.buildFHSEnv (
+    #    base
+    #    // {
+    #      name = "fhs";
+    #      targetPkgs =
+    #        pkgs:
+    #        # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
+    #        # lacking many basic packages needed by most software.
+    #        # Therefore, we need to add them manually.
+    #        #
+    #        # pkgs.appimageTools provides basic packages required by most software.
+    #        (base.targetPkgs pkgs)
+    #        ++ (with pkgs; [
+    #          pkg-config
+    #          ncurses
+    #          chromium
+    #          python3
+    #          # Feel free to add more packages here if needed.
+    #        ]);
+    #      profile = "export FHS=1";
+    #      runScript = "bash";
+    #      extraOutputsToInstall = [ "dev" ];
+    #    }
+    #  )
+    #)
   ];
 
   fonts.packages = with pkgs; [
