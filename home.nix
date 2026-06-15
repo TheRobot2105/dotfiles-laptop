7f2640e43d17392b5e6d16954165d9ed7e7f2994
Felix Kimmel <felix.kimmel@web.de>
2026-06-15 13:43:47 +0200

    updated lockfile

--- home.nix
+++ home.nix
@@ -64,6 +64,8 @@
       nodejs
       rendercv
       inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+      radiotray-ng
+      rapid-photo-downloader
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
