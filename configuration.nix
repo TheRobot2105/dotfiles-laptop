5eee19ae35f95e363702c7355c799556e2608b28
Felix Kimmel <felix.kimmel@web.de>
2026-06-18 20:39:21 +0200

    changed lanzaboote to own flake because of error

--- configuration.nix
+++ configuration.nix
@@ -206,35 +206,35 @@
     yubioath-flutter
     syncthingtray
     sbctl
-    #(
-    #  let
-    #    base = pkgs.appimageTools.defaultFhsEnvArgs;
-    #  in
-    #  pkgs.buildFHSEnv (
-    #    base
-    #    // {
-    #      name = "fhs";
-    #      targetPkgs =
-    #        pkgs:
-    #        # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
-    #        # lacking many basic packages needed by most software.
-    #        # Therefore, we need to add them manually.
-    #        #
-    #        # pkgs.appimageTools provides basic packages required by most software.
-    #        (base.targetPkgs pkgs)
-    #        ++ (with pkgs; [
-    #          pkg-config
-    #          ncurses
-    #          chromium
-    #          python3
-    #          # Feel free to add more packages here if needed.
-    #        ]);
-    #      profile = "export FHS=1";
-    #      runScript = "bash";
-    #      extraOutputsToInstall = [ "dev" ];
-    #    }
-    #  )
-    #)
+    (
+      let
+        base = pkgs.appimageTools.defaultFhsEnvArgs;
+      in
+      pkgs.buildFHSEnv (
+        base
+        // {
+          name = "fhs";
+          targetPkgs =
+            pkgs:
+            # pkgs.buildFHSUserEnv provides only a minimal FHS environment,
+            # lacking many basic packages needed by most software.
+            # Therefore, we need to add them manually.
+            #
+            # pkgs.appimageTools provides basic packages required by most software.
+            (base.targetPkgs pkgs)
+            ++ (with pkgs; [
+              pkg-config
+              ncurses
+              chromium
+              python3
+              # Feel free to add more packages here if needed.
+            ]);
+          profile = "export FHS=1";
+          runScript = "bash";
+          extraOutputsToInstall = [ "dev" ];
+        }
+      )
+    )
   ];
 
   fonts.packages = with pkgs; [
--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v1.0.0";
+      url = "github:therobot2105/lanzaboote/v1.0.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
