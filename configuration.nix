264a1c8cedda24f61a2f1645ca29318387a51f29
Felix Kimmel <felix.kimmel@web.de>
2026-07-14 20:42:42 +0200

    changed to correct texlive scheme

--- configuration.nix
+++ configuration.nix
@@ -209,7 +209,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     nixd
-    texlive.combined.scheme-full
+    texliveFull
     git
     git-lfs
     filezilla
